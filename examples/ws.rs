b1c5239a8d774b7decfdd4823174023bf420cca7
utx0 <utx0@protonmail.com>
2021-09-03 16:48:06 +1000

    Updated borrowing and cleaned up ws.rs file

--- examples/ws.rs
+++ examples/ws.rs
@@ -1,43 +1,33 @@
 use anyhow::Result;
 use binance_async::model::websocket::Subscription;
-use binance_async::{Binance, BinanceWebsocket};
+use binance_async::BinanceWebsocket;
 use futures::TryStreamExt;
-use std::env::var;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing::subscriber::set_global_default(tracing_subscriber::FmtSubscriber::new()).unwrap();
 
-    let api_key_user = var("BINANCE_KEY")?;
-    let api_secret_user = var("BINANCE_SECRET")?;
+    println!("Data Stream Started ...");
 
-    let bn = Binance::with_credential(&api_key_user, &api_secret_user);
-    match bn.user_stream_start().await {
-        Ok(_) => {
-            println!("Data Stream Started ...");
+    let mut ws = BinanceWebsocket::default();
 
-            let mut ws = BinanceWebsocket::default();
+    for sub in vec![
+        // Subscription::Ticker("btcusdt.to_string()),
+        // Subscription::AggregateTrade("btcusdt.to_string()),
+        // Subscription::Candlestick("btcusdt".to_string(), "1m".to_string()),
+        // Subscription::Depth("btcusdt".to_string()),
+        // Subscription::MiniTicker("btcusdt".to_string()),
+        // Subscription::OrderBook("btcusdt".to_string(), 10),
+        Subscription::Trade("btcusdt".to_string()),
+        // Subscription::UserData(listen_key),
+        // Subscription::MiniTickerAll,
+        // Subscription::TickerAll,
+    ] {
+        ws.subscribe(&sub).await?;
+    }
 
-            for sub in vec![
-                // Subscription::Ticker("btcusdt.to_string()),
-                // Subscription::AggregateTrade("btcusdt.to_string()),
-                // Subscription::Candlestick("btcusdt".to_string(), "1m".to_string()),
-                // Subscription::Depth("btcusdt".to_string()),
-                // Subscription::MiniTicker("btcusdt".to_string()),
-                // Subscription::OrderBook("btcusdt".to_string(), 10),
-                Subscription::Trade("btcusdt".to_string()),
-                // Subscription::UserData(listen_key),
-                // Subscription::MiniTickerAll,
-                // Subscription::TickerAll,
-            ] {
-                ws.subscribe(sub).await?;
-            }
-
-            while let Some(msg) = ws.try_next().await? {
-                println!("\n\n{:#?}", msg)
-            }
-        }
-        Err(e) => println!("Error obtaining userstream: {}", e),
+    while let Some(msg) = ws.try_next().await? {
+        println!("\n\n{:#?}", msg)
     }
 
     Ok(())
